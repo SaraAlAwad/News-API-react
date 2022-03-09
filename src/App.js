2f81f03918e98f40dccbd8533136f538ec1898db
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-03-09 22:58:17 +0100

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NewsApi from './component/NewsApi';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>News by Super(code)</h1>
+      <NewsApi />
     </div>
   );
 }
 
 export default App;
+//https://newsapi.org/
---
+++ src/component/NewsApi.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react"
+
+const NewsApi = () => {
+
+    console.log('Effect did render')
+
+    const [newType, setNewType] = useState('articles')
+
+    const [jsonArr, setJsonArr] = useState([])
+
+    // const[lang, setNewLang] = useState('de')
+
+
+    useEffect(() => {
+        let loaded = true
+        fetch(`https://newsapi.org/v2/everything?q=${newType}&language=en&apiKey=33957bdcbea740f8ab8092438e8b380e`)
+            .then(response => response.json())
+            .then(json => {
+                if (loaded) {
+                    console.log(json.articles)
+                    setJsonArr(json.articles)
+                }
+            })
+        return () => {
+            loaded = false
+            console.log('process stopped');
+        }
+    }, [newType])
+
+
+
+    return (
+        <>
+            <section>
+                <button onClick={() => setNewType('ukraine')}>Ukraine</button>
+                <button onClick={() => setNewType('business')}>business</button>
+                <button onClick={() => setNewType('lifestyle')}>Lifestyle</button>
+                <button onClick={() => setNewType('bitcoin')}>Bitcoin</button>
+                <button onClick={() => setNewType('economy')}>Economy</button>
+                <button onClick={() => setNewType('health')}>Health</button>
+                <button onClick={() => setNewType('coronavirus')}>coronavirus</button>
+            </section>
+
+
+            {jsonArr.map((items) => {
+                return (
+                    <article>
+                        <div className="image">
+                            <img src={items.urlToImage} alt="no-photo" />
+                        </div>
+                        <div className="content">
+                            <p className="title">{items.title}</p>
+                            <p className="text">{items.content}</p>
+                            <p className="date">{items.publishedAt}</p>
+                            <a href={items.url}>Read more</a></div>
+                    </article>
+                )
+            })}
+
+        </>
+    )
+}
+
+export default NewsApi
